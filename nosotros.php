56e204292eac7dd5d25bec436bd37b917f51c2e8
AgustinElizalde <agustinelizalde93@gmail.com>
2023-11-01 02:39:46 -0300

    Se actualizo el sistema con todas sus funcionalidades

---
+++ nosotros.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/style.css">
+        <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <title>GENNS|Contacto</title>
+</head>
+<body>
+
+    <header>
+        <a href="index.php" class="logo"><img class="log" src="img/GENNS-PhotoRoom.png-PhotoRoom.png" alt=""></i>GENNS</a>
+
+        <div class='bx bx-menu' id="menu-icon"></div>
+
+        <ul class="navbar">
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="promedio.php">Promedios</a></li>
+            <li><a href="nosotros.ph">Nosotros</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
+        </ul>
+
+        <div class="header-btn">
+            <a href="login.php" class="login">Iniciar Sesion</a>
+        </div>
+
+    </header>
+    <main>
+        <br>
+        <br>
+        
+        <div class="wrapper">
+    <h1>GENNS</h1>
+    <div class="our_team">
+        <div class="team_member">
+          <div class="member_img">
+             <img src="https://i.imgur.com/2Necikc.png" alt="our_team">
+            <div class="social_media">
+               <div class="facebook item"><i class="fab fa-facebook-f"></i></div>
+               <div class="twitter item"><i class="fab fa-twitter"></i></div>
+               <div class="instagram item"><i class="fab fa-instagram"></i></div>
+             </div>
+          </div>
+          <h3>Agustin Elizalde</h3>
+          <h2>Lider</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione perspiciatis, error deleniti quaerat beatae doloribus incidunt excepturi. Fugit deleniti accusantium neque hic quidem voluptatibus cumque.</p>
+        </div>
+        <div class="team_member">
+           <div class="member_img">
+             <img src="https://i.imgur.com/JzUIF4o.png" alt="our_team">
+             <div class="social_media">
+               <div class="facebook item"><i class="fab fa-facebook-f"></i></div>
+               <div class="twitter item"><i class="fab fa-twitter"></i></div>
+               <div class="instagram item"><i class="fab fa-instagram"></i></div>
+             </div>
+          </div>
+          <h3>Lucio Gauna</h3>
+          <h2>Documentacion</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione perspiciatis, error deleniti quaerat beatae doloribus incidunt excepturi. Fugit deleniti accusantium neque hic quidem voluptatibus cumque.</p>
+        </div>
+        <div class="team_member">
+           <div class="member_img">
+             <img src="https://i.imgur.com/JzUIF4o.png" alt="our_team">
+             <div class="social_media">
+               <div class="facebook item"><i class="fab fa-facebook-f"></i></div>
+               <div class="twitter item"><i class="fab fa-twitter"></i></div>
+               <div class="instagram item"><i class="fab fa-instagram"></i></div>
+             </div>
+          </div>
+          <h3>Ignacio Stegmann</h3>
+          <h2>Diagramas/Tester</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione perspiciatis, error deleniti quaerat beatae doloribus incidunt excepturi. Fugit deleniti accusantium neque hic quidem voluptatibus cumque.</p>
+      </div>
+        <div class="team_member">
+           <div class="member_img">
+             <img src="https://i.imgur.com/Ctwf8HA.png" alt="our_team">
+             <div class="social_media">
+               <div class="facebook item"><i class="fab fa-facebook-f"></i></div>
+               <div class="twitter item"><i class="fab fa-twitter"></i></div>
+               <div class="instagram item"><i class="fab fa-instagram"></i></div>
+             </div>
+          </div>
+          <h3>Franco Navarro</h3>
+          <h2>Front End</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione perspiciatis, error deleniti quaerat beatae doloribus incidunt excepturi. Fugit deleniti accusantium neque hic quidem voluptatibus cumque.</p>
+      </div>
+        <div class="team_member">
+           <div class="member_img">
+             <img src="https://i.imgur.com/A1Fjq0d.png" alt="our_team">
+             <div class="social_media">
+               <div class="facebook item"><i class="fab fa-facebook-f"></i></div>
+               <div class="twitter item"><i class="fab fa-twitter"></i></div>
+               <div class="instagram item"><i class="fab fa-instagram"></i></div>
+             </div>
+          </div>
+          <h3>Franco Nuñez</h3>
+          <h2>Back End</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione perspiciatis, error deleniti quaerat beatae doloribus incidunt excepturi. Fugit deleniti accusantium neque hic quidem voluptatibus cumque.</p>
+      </div>  
+    </div>
+</div>
+        <br>
+        <br>
+    </main>
+    <br>
+
+    <footer>
+        <div class="container__footer">
+            <div class="box__footer">
+                <div class="logof">
+                    <a href="index.php"><img src="img/GENNS-PhotoRoom.png-PhotoRoom.png" alt=""></a>
+                </div>
+                <div class="terms">
+                    <p></p>
+                </div>
+            </div>
+            <div class="box__footer">
+                <h2>Info</h2>
+                <a href="#">Empleados</a>
+                <a href="#">Nosotros</a>
+                <a href="#">Ayuda</a>
+            </div>
+
+            <div class="box__footer">
+                <h2>Compañia</h2>
+                <a href="#">Empleo</a>
+                <a href="#">Servicios</a>
+                <a href="#">Politica y Privacidad</a>
+            </div>
+
+            <div class="box__footer">
+                <h2>Redes Sociales</h2>
+                <a href="#"> <i class="fab fa-facebook-square"></i> Facebook</a>
+                <a href="#"><i class="fab fa-twitter-square"></i> Twitter</a>
+                <a href="#"><i class="fab fa-instagram-square"></i> Instagram</a>
+            </div>
+
+        </div>
+    </footer>
+
+    <script src="acciones/js.js"></script>
+
+</body>
+</html>
